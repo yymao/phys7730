270d07ac887f2d6ab460424b468bc9b010037dfe
Yao-Yuan Mao <yymao.astro@gmail.com>
2024-01-09 19:37:19 -0700

    update

--- build.sh
+++ build.sh
@@ -1,23 +1,27 @@
 #!/bin/sh
 
 while [ $# -gt 0 ]; do
-    case $1 in
-        -c|--clean)
-            CLEAN=1
-            ;;
-        -p|--push|--publish)
-            PUSH=1
-            ;;
-        -v|--view)
-            VIEW=1
-            ;;
-    esac
-    shift
+  case $1 in
+    -c|--clean)
+      CLEAN=1
+      ;;
+    -p|--push|--publish)
+      PUSH=1
+      ;;
+    -v|--view)
+      VIEW=1
+      ;;
+  esac
+  shift
 done
 
 CONDABIN='/home/yymao/miniforge3/bin/conda'
 eval "$($CONDABIN shell.bash hook)"
 conda activate 7730
+
+if [ -z "$PUSH" ] ; then
+  sed -i '/^exclude_patterns/s/, *"wip"]/]/' _config.yml
+fi
 
 NAME=$(basename $(pwd))
 cd ..
@@ -31,6 +35,7 @@ jupyter-book build $NAME
 cd $NAME
 
 PAGE='_build/html/index.html'
+HTML_DIR=$(dirname $PAGE)
 
 if [ ! -z "$VIEW" ] ; then
   xdg-open $PAGE >/dev/null 2>&1
@@ -40,8 +45,10 @@ if [ ! -z "$PUSH" ] ; then
   while true; do
     read -p "Do you want to publish the current build? [y/N] " ans
     case $ans in
-      [Yy]|[Yy]es|YES ) ghp-import -n -p -f $(dirname $PAGE); break;;
+      [Yy]|[Yy]es|YES ) rm -r $HTML_DIR/wip; ghp-import -n -p -f $HTML_DIR; break;;
       * ) exit;;
     esac
   done
 fi
+
+sed -i '/"wip"/!s/^\(exclude_patterns.*\)]/\1, "wip"]/' _config.yml
