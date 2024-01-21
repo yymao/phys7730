c567a71c6956ad9bb336dd01b2e08eaa1f2e1d8a
Yao-Yuan Mao <yymao.astro@gmail.com>
2024-01-20 17:04:33 -0700

    update configs

--- build.sh
+++ build.sh
@@ -19,36 +19,27 @@ CONDABIN='/home/yymao/miniforge3/bin/conda'
 eval "$($CONDABIN shell.bash hook)"
 conda activate 7730
 
-if [ -z "$PUSH" ] ; then
-  sed -i '/^exclude_patterns/s/, *"wip"]/]/' _config.yml
-fi
-
 NAME=$(basename $(pwd))
 cd ..
-
 if [ ! -z "$CLEAN" ] ; then
   jupyter-book clean $NAME
 fi
-
 jupyter-book build $NAME
-
 cd $NAME
 
 PAGE='_build/html/index.html'
-HTML_DIR=$(dirname $PAGE)
-
 if [ ! -z "$VIEW" ] ; then
   xdg-open $PAGE >/dev/null 2>&1
 fi
 
 if [ ! -z "$PUSH" ] ; then
-  while true; do
-    read -p "Do you want to publish the current build? [y/N] " ans
-    case $ans in
-      [Yy]|[Yy]es|YES ) rm -r $HTML_DIR/wip; ghp-import -n -p -f $HTML_DIR; break;;
-      * ) exit;;
-    esac
-  done
-fi
+  if [ "$(git branch --show-current)" != "main" ] ; then
+    echo "Cannot publish webpages when not on the main branch! Abort!"
+    exit
+  fi
 
-sed -i '/"wip"/!s/^\(exclude_patterns.*\)]/\1, "wip"]/' _config.yml
+  read -p "Do you want to publish the current build? [y/N] " ans
+  case $ans in
+    [Yy]|[Yy]es|YES ) ghp-import -n -p -f $(dirname $PAGE);;
+  esac
+fi
