afc1676dc69819fdccae987aae8010e75439a9fa
Yao-Yuan Mao <yymao.astro@gmail.com>
2025-01-04 19:03:46 -0700

    update

--- build.sh
+++ build.sh
@@ -8,9 +8,6 @@ while [ $# -gt 0 ]; do
     -c|--clean)
       CLEAN=1
       ;;
-    -p|--push|--publish)
-      PUSH=1
-      ;;
     -v|--view)
       VIEW=1
       ;;
@@ -18,24 +15,14 @@ while [ $# -gt 0 ]; do
   shift
 done
 
+EXE='.venv/bin/jupyter-book'
+
 if [ ! -z "$CLEAN" ] ; then
-  uv run jupyter-book clean .
+  $EXE clean .
 fi
-uv run jupyter-book build $ALL .
+$EXE build $ALL .
 
 PAGE='_build/html/index.html'
 if [ ! -z "$VIEW" ] ; then
   xdg-open $PAGE >/dev/null 2>&1
-fi
-
-if [ ! -z "$PUSH" ] ; then
-  if [ "$(git branch --show-current)" != "main" ] ; then
-    echo "Cannot publish webpages when not on the main branch! Abort!"
-    exit
-  fi
-
-  read -p "Do you want to publish the current build? [y/N] " ans
-  case $ans in
-    [Yy]|[Yy]es|YES ) ghp-import -n -p -f $(dirname $PAGE);;
-  esac
 fi
